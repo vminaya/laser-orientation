99dd2536ff7055f38b7559bde0cbc13969ca185c
vmm <82240140+vminaya@users.noreply.github.com>
2022-07-12 11:05:38 -0400

    my first R-script
    store info of first commands

---
+++ orientation1.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+d <- read_csv("data/sci-online-classes.csv")
+d
+5+10
+number_1 <-1
+number_2 <-2
+add_numbers <-function(number_1, number_2)(number_1+number_2)
+add_numbers(5,10)
+add_numbers(6,10
+          )
